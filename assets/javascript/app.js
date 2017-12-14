25b99c413e9817a84d6129d1835cee35563cbca4
Evan Darst <evan.l.darst@gmail.com>
2017-12-13 20:24:33 -0500

    pseudo coding some JS while I look into some stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//Copyright 2017 Evan Darst
+//Use limited to educational use only
+
+//Set a timer for the game
+
+
+
+//global variables for the number of correct and incorrect responses
+
+
+
+//ALL GAME FUNCTIONS
+
+
+
+//set the on-click function for the begin button
+
+
+
+//un-hide the game_container
+
+
+
