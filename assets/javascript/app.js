0b49be278f0d46d84c34947997cf117aed25332a
Evan Darst <evan.l.darst@gmail.com>
2017-12-14 00:04:41 -0500

    Added all JS for validating user responses and changing right and wrong count

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -77,6 +77,218 @@ function startCountdown(){
 
 function endTime(){
 
+//Check what's entered by the user into each question
 
-	
+	var q1 = $('input:radio[name="q1"]:checked').val();
+	var q2 = $('input:radio[name="q2"]:checked').val();
+	var q3 = $('input:radio[name="q3"]:checked').val();
+	var q4 = $('input:radio[name="q4"]:checked').val();
+	var q5 = $('input:radio[name="q5"]:checked').val();
+	var q6 = $('input:radio[name="q6"]:checked').val();
+	var q7 = $('input:radio[name="q7"]:checked').val();
+	var q8 = $('input:radio[name="q8"]:checked').val();
+	var q9 = $('input:radio[name="q9"]:checked').val();
+	var q10 = $('input:radio[name="q10"]:checked').val();
+
+//increase the right or wrong count depending on the user's input
+//Is there a better way to do this? This took forever and is hard to read
+
+//Question 1
+
+	if(q1 == undefined){
+
+		wrongCount++;
+
 }
+
+	else if(q1 == "Beer"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 2
+
+	if(q2 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q2== "Eat the frogs"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 3
+
+	if(q3 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q3 == "Must bounce when dropped"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 4
+
+	if(q4 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q4 == "Curse or using profane language"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 5
+
+	if(q5 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q5 == "Cannibalism"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 6
+
+	if(q6 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q6 == "Catch fish"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 7
+
+	if(q7 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q7 == "Rabbits"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 8
+
+	if(q8 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q8 == "Golf balls"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 9
+
+	if(q9 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q9 == "Throwing objects"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
+
+//Question 10
+
+	if(q10 == undefined){
+
+		wrongCount++;
+
+}
+
+	else if(q10 == "Play pinball machines"){
+
+		rightCount++;
+
+	}
+
+	else{
+
+		wrongCount++;
+
+	}
